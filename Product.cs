cd56f5c2c32ad951f9007550594d68ff48847f08
Sagira <102478767+1Sagira@users.noreply.github.com>
2025-10-21 23:07:54 +0200

    DotNet-WebAPI-Frontend-CRUD

--- Product.cs
+++ Product.cs
@@ -6,4 +6,6 @@ public class Product
 	public decimal Price { get; set; } 
 	public DateTime CreatedAt { get; set; } 
 	public string Category { get; set; } 
+	public string Description { get; set; } 
+    public bool InStock { get; set; }
 }
