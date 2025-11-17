5acbcbdf0924e8745aa35bbc743f93eb48c8a7fa
saiirasu <movchan2005@gmail.com>
2025-11-17 16:49:27 +0100

    fix: Do not run migrations when using InMemory database for tests

--- MyCrudApp/Program.cs
+++ MyCrudApp/Program.cs
@@ -1,10 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -12,11 +10,16 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    
-    dbContext.Database.Migrate();
+
+
+    if (!dbContext.Database.IsInMemory())
+    {
+        dbContext.Database.Migrate();
+    }
 }
 
 if (app.Environment.IsDevelopment())
@@ -26,7 +29,6 @@ if (app.Environment.IsDevelopment())
 }
 
 //app.UseHttpsRedirection();
-
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
